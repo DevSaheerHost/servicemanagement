a8ceae7793c850ffe08e9f0d389f86ca6b5a0474
devsaheerhost <sheerbabu549@gmail.com>
2026-02-23 11:06:10 +0530

    bottom nav animation improvement, ux fixes

--- app.js
+++ app.js
@@ -1179,7 +1179,10 @@ function router() {
 ========================= */
 
 window.addEventListener("hashchange", router);
-window.addEventListener("load", router);
+window.addEventListener("load", ()=>{
+  router()
+  document.querySelector('#bottomNav').style.display='flex'
+});
 
 // bottom nav switch function 
 
